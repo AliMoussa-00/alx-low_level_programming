31ec42ae5fd37285d5a59ff9b395d223b1738503
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-15 08:22:14 -0700

    task .101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (error)
+ */
+int main(void)
+{
+	char s = "and that piece of art is useful- Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s", s);
+	return (1);
+}
