64dae7af758e128b74a4fcda07e3ae653303d52a
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-16 10:53:28 -0700

    task .5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always successfull
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
