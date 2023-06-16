d5ad744fd539e8e2d664a2ce2de7c8a23305ebf0
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-16 11:34:32 -0700

    task .9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always successfull
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
