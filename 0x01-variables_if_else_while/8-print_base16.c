b1f0cb40afb3d07a73d7016fcfe026265affa9e5
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-16 11:28:22 -0700

    task .8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always successfull
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 16)
+	{
+		if (n < 10)
+			putchar(n + '0');
+		else
+			putchar(n - 10 + 'a');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
