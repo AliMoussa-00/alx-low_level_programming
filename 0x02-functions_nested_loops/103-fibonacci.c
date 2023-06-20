e2a1bfdab63465b5ac850f36b0bbde4d22f27ee8
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-20 16:30:21 -0700

    task .103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	unsigned long sum = 0, s = 2, pp = 1, p = 2, i = 4000000;
 
-	while (sum < i)
+	while (sum <= i)
 	{
 		sum = p + pp;
 		if (sum % 2 == 0)
@@ -17,6 +17,6 @@ int main(void)
 		pp = p;
 		p = sum;
 	}
-	printf("%lu\n", sum);
+	printf("%lu\n", s);
 	return (0);
 }
