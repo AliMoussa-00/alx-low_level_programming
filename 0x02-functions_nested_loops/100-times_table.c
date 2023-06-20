ae8b5fd1039d7d5784c489994645b0acc7fb8b97
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-20 13:23:23 -0700

    task .100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,10 +32,16 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(k + '0');
 				}
-				else
+				else if (k <= 99)
 				{
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k % 100)/10) + '0');
+					_putchar(((k % 100)%10) + '0');
 				}
 
 				j++;
