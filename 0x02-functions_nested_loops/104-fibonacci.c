1873488f87cb5cc9921490eeffcb848576dd7212
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-20 16:38:26 -0700

    task .104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i = 3, j = 4;
+
+	long double sum = 0, pp = 1, p = 2;
+
+	printf("1, 2, ");
+
+	while (i <= 98)
+	{
+		sum = p + pp;
+		printf("%Lf", sum);
+		if (j != 99)
+			printf(", ");
+
+		pp = p;
+		p = sum;
+		j++;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
