d431604766e0e17b87c99e1e02d6e1ae5924380b
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-20 04:16:05 -0700

    task .0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char s[] = "_putchar";
+
+	for (int i = 0; i < 9 ; i++)
+	{
+		_putchar(i);
+	}
+
 	_putchar('\n');
 
 	return (0);
