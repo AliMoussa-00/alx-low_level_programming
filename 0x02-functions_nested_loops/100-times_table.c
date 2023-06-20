733593f1e8f8d5329701e45d821ebd7e3d3b1291
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-20 13:14:38 -0700

    task .100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,12 +14,12 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		while (i < n)
+		while (i <= n)
 		{
 			int j = 1;
 
 			_putchar('0');
-			while (j < n)
+			while (j <= n)
 			{
 				int k = i * j;
 
