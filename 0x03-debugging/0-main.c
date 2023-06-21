5b5d7597881c650d3c8f90072b5be2d4fa14dd7f
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-21 04:09:55 -0700

    task .0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* positive_or_negative - test if nulber is positive.
+* @i: number to test
+*
+* Return: void.
+*/
+void positive_or_negative(int i)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
