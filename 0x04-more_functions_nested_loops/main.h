206ded19fb3b7167bdca34ba63dea1c2be29481a
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-22 03:11:21 -0700

    task .1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,13 @@
 * Return: 1 if true else 0.
 */
 int _isupper(int c);
+
+/**
+* _isupper - a function that checks for a digit (0 through 9).
+* @c: the char to check
+*
+* Return: 1 if true else 0.
+*/
+int _isdigit(int c);
+
 #endif
