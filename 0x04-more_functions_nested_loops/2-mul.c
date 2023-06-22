ce83971bfe511ea4620de9148ee2f725933d9c57
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-22 03:16:09 -0700

    task .2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - a function that multiplies two integers.
+* @a: num1
+* @b: num2
+*
+* Return: 1 if true else 0.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,4 +17,14 @@ int _isupper(int c);
 */
 int _isdigit(int c);
 
+/**
+* mul - a function that multiplies two integers.
+* @a: num1
+* @b: num2
+*
+* Return: 1 if true else 0.
+*/
+int mul(int a, int b);
+
+
 #endif
