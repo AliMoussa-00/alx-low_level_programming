dbbc0e5f45ef60c9f29fbe91a698de9f59f7f2c3
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-22 03:28:29 -0700

    task .4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -34,4 +34,11 @@ int mul(int a, int b);
 */
 void print_numbers(void);
 
+/**
+* print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4.
+*
+* Return: void.
+*/
+void print_most_numbers(void);
+
 #endif
