69e85d1da4a2b059fed5a428709680041b60b6e6
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-22 03:05:56 -0700

    task .0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+* _isupper - a function that checks for uppercase character.
+* @c: the char to check
+*
+* Return: 1 if true else 0.
+*/
+int _isupper(int c)
+{
+	return ((c <= 90 && c >= 65) ? 1 : 0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _isupper - a function that checks for uppercase character.
+* @c: the char to check
+*
+* Return: 1 if true else 0.
+*/
+int _isupper(int c);
+#endif
