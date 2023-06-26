9913981edb06f93de4c858f0e0a0536ec222f7c8
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-26 04:22:16 -0700

    task .2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -25,4 +25,13 @@ void reset_to_98(int *n);
 */
 
 void swap_int(int *a, int *b);
+/**
+* _strlen - a function that returns the length of a string.
+* @s: pointer to char
+*
+* Return: int.
+*/
+
+int _strlen(char *s);
+
 #endif
