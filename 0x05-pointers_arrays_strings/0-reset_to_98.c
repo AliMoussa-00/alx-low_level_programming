569a9db82662dcf306ab673349ed9f04431094cb
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-26 04:11:21 -0700

    task .0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - print_number.
+* @n: pointer to int
+*
+* Return: void.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - print
+ * @c: char
+ *
+ * Return: char
+ */
+int _putchar(char c);
+
+/**
+* reset_to_98 - print_number.
+* @*n: pointer to int
+*
+* Return: void.
+*/
+
+void reset_to_98(int *n);
+
+#endif
