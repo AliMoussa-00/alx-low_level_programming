2863f1f9dc816e24d745cbf47463f3cd9a39ec20
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-06-29 09:28:34 -0700

    task .101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,18 @@
  */
 void print_number(int n)
 {
+	unsigned int k = n;
+
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n *= -1;
+		k = n;
 	}
 
-	if (n / 10 != 0)
+	if (k / 10 != 0)
 	{
 		print_number(n / 10);
 	}
-	putchar((n % 10) + '0');
+	_putchar((k % 10) + '0');
 }
