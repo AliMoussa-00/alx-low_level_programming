d56ea5a72f206e5e21e3b56976626de640597b01
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-07-03 07:05:23 -0700

    task .5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,37 +11,20 @@
 char *_strstr(char *haystack, char *needle)
 {
 	char *x = haystack, *y = needle;
-	int i = 0, k;
-
-	if (*y == '\0')
-	{
-		while (*x != '\0')
-			x++;
-		return (x);
-	}
-
-	while (*y != '\0')
-	{
-		i++;
-		y++;
-	}
-	y = needle;
 
 	while (*x != '\0')
 	{
 		if (*x == *y)
 		{
-			char *a = x, *b = y, *f = x;
+			char *a = x, *b = y;
 
-			k = 0;
 			while (*a != '\0' && *b != '\0' && *a == *b)
 			{
-				k++;
 				a++;
 				b++;
 			}
-			if (k == i)
-				return (f);
+			if (*b == '\0')
+				return (x);
 		}
 		x++;
 	}
