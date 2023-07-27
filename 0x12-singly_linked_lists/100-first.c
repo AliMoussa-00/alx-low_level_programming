f56a14b7ca246170acea482ff70d9100999c8a50
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-07-27 05:38:34 -0700

    task .100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+void statrt_up_func(void) __attribute__((constructor));
+
+/**
+ * start_up_func - executes before main func
+ *
+ * Return: void.
+ */
+void start_up_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
