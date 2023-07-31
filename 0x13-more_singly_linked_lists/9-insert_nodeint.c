45985795d4022d863bf12e0d5475874b1c8fb252
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-07-31 13:50:38 -0700

    task ;9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head && idx > 0)
 	{
-		printf("§§\n");
 		while (current && i + 1 < idx)
 		{
 			current = current->next;
