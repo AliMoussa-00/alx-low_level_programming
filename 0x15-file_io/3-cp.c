d20e8e396415e0dbed080794d58fa4085a2fd7b0
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-08-07 06:54:52 -0700

    taaks. 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,11 +42,16 @@ void open_to(int *to, int *from, char *name)
 void close_files(int *from, int *to)
 {
 	if (close(*from) == -1)
+	{
 		dprintf(2, "Error: Can't close fd %i\n", *from);
+		exit(100);
+	}
 	if (close(*to) == -1)
+	{
 		dprintf(2, "Error: Can't close fd %i\n", *to);
+		exit(100);
+	}
 
-	exit(100);
 }
 /**
  * read_write_error - if there is an error while reading or writting.
